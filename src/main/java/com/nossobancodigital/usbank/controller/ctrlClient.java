a56baaee4d38330720503c8dcbf76707671bbe46
eduardofrederico <eduardo.frederico@icloud.com>
2020-10-09 16:10:20 -0300

    Adicionadas dependências ao .xml; Criação da classe do cliente; Realizadas alterações no Controller; e Ajustes no .html e .css;

--- src/main/java/com/nossobancodigital/usbank/controller/ctrlClient.java
+++ src/main/java/com/nossobancodigital/usbank/controller/ctrlClient.java
@@ -2,63 +2,31 @@ package com.nossobancodigital.usbank.controller;
 
 import java.util.Date;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+
+@Controller
+@RequestMapping(/clients)
 public class ctrlClient {
 
-    private String client_name;
-    private String client_lastname;
-    private String client_email;
-    private String client_cnh;
-    private Date client_birthday;
-    private String client_cpf;
-
-    public String getClient_name() {
-        return client_name;
+    @RequestMapping(value="/novo", method = RequestMethod.GET)
+    public ModelAndView novo(Clients client) {
+        ModelAndView mv = new ModelAndView("CadastroCliente");
+        return mv;
     }
 
-    public void setClient_name(String client_name) {
-        this.client_name = client_name;
+    @RequestMapping(value = "novo", method = RequestMethod.POST)
+    public ModelAndView salvar(Clients client, RedirectAttributes attributes) {
+        ModelAndView mv = new ModelAndView("redirect:/clients/novo");
+        attributes.addFlashAttribute("mensagem","Seus dados foram enviados para análise e em breve retornaremos a sua solicitação de abertura de conta");
+        return mv;
     }
-
-    public String getClient_lastname() {
-        return client_lastname;
-    }
-
-    public void setClient_lastname(String client_lastname) {
-        this.client_lastname = client_lastname;
-    }
-
-    public String getClient_email() {
-        return client_email;
-    }
-
-    public void setClient_email(String client_email) {
-        this.client_email = client_email;
-    }
-
-    public String getClient_cnh() {
-        return client_cnh;
-    }
-
-    public void setClient_cnh(String client_cnh) {
-        this.client_cnh = client_cnh;
-    }
-
-    public Date getClient_birthday() {
-        return client_birthday;
-    }
-
-    public void setClient_birthday(Date client_birthday) {
-        this.client_birthday = client_birthday;
-    }
-
-    public String getClient_cpf() {
-        return client_cpf;
-    }
-
-    public void setClient_cpf(String client_cpf) {
-        this.client_cpf = client_cpf;
-    }
+    
+    
 }
